ab25002c99b3af302eac0cb004e39b609aa1a4cb
vollmer <kotlincook@gmx.de>
2018-12-31 01:41:17 +0100

    finished

---
+++ src/main/kotlin/Benford.kt
@@ -0,0 +1,45 @@
+import java.math.BigInteger
+import java.math.BigInteger.*
+
+val NO_OF_DIGITS = 20
+val MAX_EXPONENT = 1000
+val NO_SAMPLES = 1000000
+
+
+fun firstDigit(number: BigInteger) = number.toString()[0].toInt() - 48
+
+fun printDestribution(array: Array<Int>) {
+    array.forEachIndexed { index, elem ->
+        println("$index: ${elem.toFloat() / NO_SAMPLES.toFloat()}" )
+    }
+}
+
+fun randomBinary() = if (Math.random() < 0.5) ZERO else ONE
+
+fun randomExponent() = (Math.random() * MAX_EXPONENT).toInt()
+
+fun randomNumber(): BigInteger {
+    var result = valueOf(0)
+
+    repeat(NO_OF_DIGITS) {
+        result = result * TWO + randomBinary()
+    }
+    repeat(randomExponent()) {
+        result *= TWO
+    }
+    return result
+}
+
+
+fun main(args: Array<String>) {
+
+    val array = Array(10) { 0 }
+
+    repeat(NO_SAMPLES) {
+        val number = randomNumber()
+        if (number != ZERO) array[firstDigit(number)]++
+    }
+
+    printDestribution(array)
+
+}
